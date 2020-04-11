df41a98e551343f4a357c2c9808bae75a1e0746b
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-04-11 16:24:29 +0800

    add: qrcode encode

--- app/controller/v1/qrcode.js
+++ app/controller/v1/qrcode.js
@@ -1,10 +1,16 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const qrcode = require('qrcode');
 
 class QRCodeController extends Controller {
     async encode() {
-        
+        const { ctx } = this;
+        ctx.validate({ text: 'string' }, ctx.query);
+        // 创建二维码
+        const data = await qrcode.toDataURL(ctx.query.text);
+        ctx.type = 'image/png';
+        ctx.body = Buffer.from(data.split(',')[1], 'base64');
     }
 
     async decode() {
--- app/router.js
+++ app/router.js
@@ -8,6 +8,6 @@ const api_version = 'v1';
 module.exports = app => {
     const { router, controller } = app;
     // QRCode
-    router.get(`/${api_version}/qrcode/encode`, controller[api_version].qrcode.encode);
-    router.get(`/${api_version}/qrcode/decode`, controller[api_version].qrcode.decode);
+    router.get(`/api/${api_version}/qrcode/encode`, controller[api_version].qrcode.encode);
+    router.get(`/api/${api_version}/qrcode/decode`, controller[api_version].qrcode.decode);
 };
